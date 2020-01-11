29b4892b739178e1f7d5cb4f120bb9c27439341f
Joshua Burt <js770676@dal.ca>
2020-01-11 14:16:57 -0400

    Uploaded and added images for the cards

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,30 @@
-public class Card {
-    public Card(int index, String suit) {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class Card {
+    private int index;
+    private String suit;
+    private BufferedImage img;
+
+    public Card(int index, String suit) throws IOException {
+        this.index = index;
+        this.suit = suit.toUpperCase();
+
+        File file = new File("img/" + index + suit.toUpperCase() + ".png");
+        img = ImageIO.read(file);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public BufferedImage getImg() {
+        return img;
     }
 }
