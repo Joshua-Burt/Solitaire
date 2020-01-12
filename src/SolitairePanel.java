b75d06392070aa3a47b2b80bef7da959f4e01b6b
Joshua Burt <js770676@dal.ca>
2020-01-11 21:30:05 -0400

    added background to files, rearranged card images

--- src/SolitairePanel.java
+++ src/SolitairePanel.java
@@ -1,7 +1,20 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class SolitairePanel extends JPanel {
+    private BufferedImage img;
+
     public SolitairePanel() {
         Deck deck = new Deck();
+
+        try {
+            File file = new File("src//img//cards//background.png");
+            img = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
